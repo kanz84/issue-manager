45f3115a1b374405d8e47081d2e30b990701f3b9
kanz84 <kanz1384@gmail.com>
2022-02-21 23:57:35 +0330

    cp on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
+#!/bin/sh
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Current Time : $current_time"
+
 rm -rf venv
 python3.6 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
+python manage.py collectstatic
+
+new_fileName=$current_time
+echo "New FileName: " "$new_fileName"
+
+mv ~/artifacts/issue-manager-pro/current ~/artifacts/issue-manager-pro/$new_fileName
+mkdir ~/artifacts/issue-manager-pro/current
+cp -r ./*  ~/artifacts/issue-manager-pro/current/
