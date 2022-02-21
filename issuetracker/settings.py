0b48df8395e1c2aceba785c88af163659da22106
kanz84 <kanz1384@gmail.com>
2022-02-22 00:05:46 +0330

    --noinput

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/home/git-auto-deploy/artifacts/issue-manager-pro/static/"
+STATIC_URL = "/static/"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
