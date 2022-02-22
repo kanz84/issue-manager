c9883ead4eb754abe9b1dace304e0722fb5a8a22
kanz84 <kanz1384@gmail.com>
2022-02-22 20:53:58 +0330

    changed CD process

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 current_time=$(date "+%Y%m%d_%H%M%S")
 echo "Current Time : $current_time"
 
-rm -rf venv
+mkdir ~/artifacts/issue-manager-pro/deploy_$new_fileName
+cp -r ./*  ~/artifacts/issue-manager-pro/deploy_$new_fileName/
+
+cd ~/artifacts/issue-manager-pro/deploy_$new_fileName/
 python3.6 -m venv venv
 . venv/bin/activate
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
@@ -12,6 +16,5 @@ python manage.py collectstatic --noinput
 new_fileName=$current_time
 echo "New FileName: " "$new_fileName"
 
-mv ~/artifacts/issue-manager-pro/current ~/artifacts/issue-manager-pro/$new_fileName
-mkdir ~/artifacts/issue-manager-pro/current
-cp -r ./*  ~/artifacts/issue-manager-pro/current/
+rm ~/artifacts/issue-manager-pro/current
+ln -s ~/artifacts/issue-manager-pro/$new_fileName ~/artifacts/issue-manager-pro/current
