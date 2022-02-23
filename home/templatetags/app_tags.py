bcd25f6f340b2da1f7af8782627c5618bc8b34e6
kanz84 <kanz1384@gmail.com>
2022-02-23 23:03:17 +0330

    check protection

--- home/templatetags/app_tags.py
+++ home/templatetags/app_tags.py
@@ -13,9 +13,9 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='gravatar')
+@register.filter(name="gravatar")
 def gravatar(user, size=35):
-    email = str(user.email.strip().lower()).encode('utf-8')
+    email = str(user.email.strip().lower()).encode("utf-8")
     email_hash = md5(email).hexdigest()
     url = "//www.gravatar.com/avatar/{0}?s={1}&d=identicon&r=PG"
     return url.format(email_hash, size)
