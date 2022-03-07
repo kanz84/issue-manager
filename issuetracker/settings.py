e47c79dc31a217db2629e2cb722b5fca268255d2
kanz84 <kanz1384@gmail.com>
2022-03-07 23:11:19 +0330

    media files

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR.parent, "media")
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
