823d9756b2f73c8264cd4b52a14d3f0a315270b8
kanz84 <kanz1384@gmail.com>
2022-05-20 02:04:32 +0430

    --suppress-orphan-warning

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ if [ ! -f $local_settings ]; then
     echo "local_settings.py created in project dir"
 fi
 
-docker-compose up -d --build --ignore-orphans
+docker-compose up -d --build
 echo "Docker image built"
 
 
