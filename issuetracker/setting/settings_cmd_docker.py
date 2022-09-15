eb23e130c93bf1a1ed1f202d9aed000518892303
kanz84 <kanz1384@gmail.com>
2022-09-16 00:19:03 +0430

    change dirs

--- issuetracker/setting/settings_cmd_docker.py
+++ issuetracker/setting/settings_cmd_docker.py
@@ -2,8 +2,11 @@ from issuetracker.settings import DATABASES
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
-
 DATABASES["default"]["HOST"] = "issue_mgr_db"
 
-LOGGING_ENABLE_LOG_FILE_HANDLER = False
+STATIC_ROOT = "/workspace/files/static"
+MEDIA_ROOT = "/workspace/files/media"
+
+LOAD_STATICS = False
 LOGGING_DB_BACKENDS_SCHEMA_LOG_LEVEL = "INFO"
+LOGGING_ENABLE_LOG_FILE_HANDLER = False
--- issuetracker/setting/settings_dev_docker.py
+++ issuetracker/setting/settings_dev_docker.py
@@ -7,4 +7,7 @@ LOGGING_FILE_LOCATION = "/workspace/files/log/app.log"
 LOGGING_ENABLE_LOG_FILE_HANDLER = True
 LOGGING_DB_BACKENDS_LOG_LEVEL = True
 
+STATIC_ROOT = "/workspace/files/static"
+MEDIA_ROOT = "/workspace/files/media"
+
 DATABASES["default"]["HOST"] = "issue_mgr_db"
