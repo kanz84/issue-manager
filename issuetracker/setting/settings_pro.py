219f565a74a23922e4477b983b369cb9e0cc9106
kanz84 <kanz1384@gmail.com>
2022-09-16 00:12:47 +0430

    redep

--- issuetracker/setting/settings_pro.py
+++ issuetracker/setting/settings_pro.py
@@ -1,6 +1,4 @@
-import os
-
-from issuetracker.settings import DATABASES
+from issuetracker.settings import DATABASES, ensure_get_file
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
@@ -13,11 +11,6 @@ LOAD_STATICS = False
 
 LOGGING_CONSOLE_LOG_LEVEL = "ERROR"
 LOGGING_ENABLE_LOG_FILE_HANDLER = True
-
-
-def ensure_get_file(path):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    return path
 
 
 LOGGING_FILE_LOCATION = ensure_get_file("/workspace/files/log/app.log")
